a490fbe8d824a3170d76375659af3840829d4aaa
Ruochen Li <liruochenv@gmail.com>
2019-11-20 21:18:30 -0500

    add projects details

--- projects.js
+++ projects.js
@@ -1,16 +1,38 @@
 const project = [
     {
-        "name": "personal blog",
-        "description": "This is a blog that I built"
+        "name": "Tone Sequencer",
+        "date": "Nov 2019",
+        "description": "This is a 'devlab' project for COMP110: Introduction to Computer Science. It will be used as future problem sets starting Spring 2020. My partner Tony and I use Tone.js music library to build a sequencer. MVC design patter is used to sepatate student code and UI.",
+        "keyword": "Typescript, HTML, CSS, Bootstrap, MVC",
+        "link": ""
+                        
+    },
+    {
+        "name": "Concept List",
+        "date": "Oct 2019",
+        "description": "This is a hackathon project from hack NC 2019. It is used to help students remember concepts by adding/deleting concept pairs and play a game to test the memory.",
+        "keyword": "Javascript, HTML, CSS, Bootstrap, JQuery",
+        "link": ""
+    },
+    {
+        "name": "Blog",
+        "date": "Jun 2018",
+        "description": "This is a blog that I built using framwork hexo from hexo.io, hosting on github page. Link: liruochen1998.github.io",
+        "link": ""
     },
     {
         "name": "Thegrep",
+        "date": "April 2019",
         "description": "tar heel egrep"
     },
     {
-        "name": "Tone Sequencer",
-        "description": "tone.js utilization"
-    }
+        "name": "",
+        "date": "",
+        "description": "",
+        "keyword": "",
+        "link": ""
+    },
+
 ]
 
 
